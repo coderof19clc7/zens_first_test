d9ce2bbcbe97044c50ea0794cf9298834666e330
coderof19clc7 <63506231+coderof19clc7@users.noreply.github.com>
2023-06-29 11:33:55 +0700

    add bonus part for algorithm

--- algorithm/README.md
+++ algorithm/README.md
@@ -1,11 +1,12 @@
 ## How I solve the problem
 To solve the required algorithmic problem, I did the following steps:
-1. Generate a list of 5 integers between 0 and 1000.
-2. Print the list (just to see the input).
-3. Call function miniMaxSum() with the list as parameter.
-4. Implement the miniMaxSum function:
-   - Prepare 3 variables: min, max and sum (all initial values are 0).
-   - Make 1 for loop through the list to find min value, max value and the sum.
-   - Print the min sum is the result of sum minus max value and max sum is the result of sum minus min value.
+1. Get 10 test cases (by generating 10 list of 5 integers between -500 and 1000 with function `randomTestCases` or using the constants list list with function `getTestCases`).
+2. Run for loop to get through all test cases.
+3. Call function `miniMaxSum` with the current list as parameter.
+4. Implement the `miniMaxSum` function:
+   - Prepare 5 variables: _min_, _max_ and _sum_ (all initial values are 0) and 2 empty lists int _evens_ and _odds_.
+   - Make 1 for loop through the list to find _min_ value, _max_ value and the _sum_, also find even and odd elements.
+   - Print the `min sum` is the result of `sum - max` value and `max sum` is the result of `sum - min` value.
+   - Print the _sum_, _min_, _max_ and the 2 lists _evens_ and _odds_.
 
 ## The code is implemented in bin/algorithm.dart file
--- algorithm/bin/algorithm.dart
+++ algorithm/bin/algorithm.dart
@@ -2,21 +2,124 @@ import 'dart:math';
 
 import 'package:algorithm/algorithm.dart' as algorithm;
 
+/* Test cases
+1
+- [590, 59, -404, 385, 3]
+--> minSum: 43, maxSum: 1037
+--> sum: 633, min: -404, max: 590,
+-->evens: [590, -404], odds: [59, 385, 3]
+
+2
+- [-118, -461, 427, -225, 588]
+--> minSum: -377, maxSum: 672,
+--> sum: 211, min: -461, max: 588,
+-->evens: [-118, 588], odds: [-461, 427, -225]
+
+3
+- [284, 230, 196, 42, 325]
+--> minSum: 752, maxSum: 1035
+--> sum: 1077, min: 42, max: 325,
+-->evens: [284, 230, 196, 42], odds: [325]
+
+4
+- [453, -473, -107, 221, 108]
+--> minSum: -251, maxSum: 675
+--> sum: 202, min: -473, max: 453,
+-->evens: [108], odds: [453, -473, -107, 221]
+
+5
+- [-123, -316, -390, 376, -440]
+--> minSum: -1269, maxSum: -453
+--> sum: -893, min: -440, max: 376,
+-->evens: [-316, -390, 376, -440], odds: [-123]
+
+6
+- [77, 588, 147, -186, -309]
+--> minSum: -271, maxSum: 626
+--> sum: 317, min: -309, max: 588,
+-->evens: [588, -186], odds: [77, 147, -309]
+
+7
+- [444, -451, -77, 37, 445]
+--> minSum: -47, maxSum: 849
+--> sum: 398, min: -451, max: 445,
+-->evens: [444], odds: [-451, -77, 37, 445]
+
+8
+- [174, -104, 189, 241, -76]
+--> minSum: 183, maxSum: 528
+--> sum: 452, min: -104, max: 241,
+-->evens: [174, -104, -76], odds: [189, 241]
+
+9
+- [224, -485, 161, 43, 304]
+--> minSum: -57, maxSum: 732
+--> sum: 147, min: -485, max: 304,
+-->evens: [224, 304], odds: [-485, 161, 43]
+
+10
+- [406, 176, -424, -307, -322]
+--> minSum: -877, maxSum: -47
+--> sum: -471, min: -424, max: 406,
+-->evens: [406, 176, -424, -322], odds: [-307]
+*/
+
+List<List<int>> randomTestCases() {
+  final random = Random();
+  final testCases = <List<int>>[];
+  for (var i = 0; i < 10; i++) {
+    final arr = List.generate(5, (_) => random.nextInt(1000 + 100) - 500);
+    testCases.add(arr);
+  }
+  return testCases;
+}
+
+List<List<int>> getTestCases() {
+  return [
+    [590, 59, -404, 385, 3],
+    [-118, -461, 427, -225, 588],
+    [284, 230, 196, 42, 325],
+    [453, -473, -107, 221, 108],
+    [-123, -316, -390, 376, -440],
+    [77, 588, 147, -186, -309],
+    [444, -451, -77, 37, 445],
+    [174, -104, 189, 241, -76],
+    [224, -485, 161, 43, 304],
+    [406, 176, -424, -307, -322],
+  ];
+}
+
 void miniMaxSum(List<int> arr) {
+  // check input
+  print('5 integers: $arr');
+
   int min = arr[0];
   int max = arr[0];
   int sum = 0;
+  final evens = <int>[], odds = <int>[];
   for (int i = 0; i < arr.length; i++) {
     if (arr[i] < min) min = arr[i];
     if (arr[i] > max) max = arr[i];
     sum += arr[i];
+    if (arr[i] == 0 || arr[i] % 2 == 0) {
+      evens.add(arr[i]);
+    } else {
+      odds.add(arr[i]);
+    }
   }
+
+  // check output
   print('${sum - max} ${sum - min}');
+
+  // bonus part
+  print('sum: $sum, min: $min, max: $max');
+  print('evens: $evens, odds: $odds\n');
 }
 
 void main(List<String> arguments) {
-  final random = Random();
-  final arr = List.generate(5, (_) => random.nextInt(1000));
-  print('5 integers: $arr');
-  miniMaxSum(arr);
+  final testCases = getTestCases();
+  final length = testCases.length;
+  for (var i = 0; i < length; i++) {
+    miniMaxSum(testCases[i]);
+  }
 }
